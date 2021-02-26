1037ba1f4f398bc7758d8c88d1526a7e1bf31112
Antonio Rodríguez Hurtado <antrodrigh@gmail.com>
2021-02-26 15:56:38 +0100

    Añadida mi parte
    La he puesto debajo del todo

--- Practica 0/practica 0.py
+++ Practica 0/practica 0.py
@@ -40,3 +40,53 @@ def integra_mc(fun, a, b, num_puntos=10000):
 
 resultado = "Nuestro resultado: " + str(integra_mc(cuadrado, 0, 5)) + " y el resultado de sicpy: " + str(integrate.quad(cuadrado, 0, 5)[0])
 print(resultado)
+
+# Parte de Toni
+#   Practica 0 
+
+#   Librerias a usar
+import random
+import scipy
+import numpy
+import time
+
+#   Funcion de prueba
+def cuadrado(x):
+    return x * x
+
+#   Funcionamiento: I = (Ndebajo / Ntotal) * (b - a) * M
+
+#   Sin uso de vectores
+def integra_mc(fun, a, b, num_puntos=100000):
+    M = fun(b)
+    base = b - a
+    nDebajo = 0
+    for i in range(num_puntos):
+        x = random.uniform(a,b)
+        y = random.uniform(0,M)
+        if(y <= fun(x)): 
+            nDebajo += 1
+    return (nDebajo/num_puntos) * base * M
+
+#   Usando vectores
+def numpy_integra_mc(fun, a, b, num_puntos=100000):
+    M = fun(b)
+    base = b - a
+    nDebajo = 0
+    x = numpy.random.uniform(a,b, num_puntos)
+    y = numpy.random.uniform(0,M, num_puntos)
+    nDebajo = sum(y <= fun(x))
+    return (nDebajo/num_puntos) * base * M
+
+#   Calculamos el tiempo de ejecucion de cada funcion y su resultado
+init = time.time()
+s = integra_mc(cuadrado, 0, 5)
+s_time = time.time() - init
+
+init = time.time()
+c = numpy_integra_mc(cuadrado, 0, 5)
+c_time = time.time() - init
+
+print("Resultado sin vectores("+str(s_time)+"s): "+ str(s))
+print("Resultado con vectores("+str(c_time)+"s): "+ str(c))
+print("Resultado con scypy: "+ str(scipy.integrate.quad(cuadrado, 0, 5)[0]))
