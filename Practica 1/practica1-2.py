359664dc6b900108ba1f435d3355b99f066448b3
miferr <mikferr99@gmail.com>
2021-03-19 11:02:39 +0100

    Update practica1-2.py

--- Practica 1/practica1-2.py
+++ Practica 1/practica1-2.py
@@ -1,43 +1,44 @@
 # Practica 1 - Parte 2
 
-#   Librerias
+# Librerias
 import numpy as np
 from pandas.io.parsers import read_csv
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
 
-#   Convertimos el .csv en un array de numpy
+#  Convertimos el .csv en un array de numpy
 valores = read_csv("ex1data2.csv", header=None).to_numpy().astype(float)
 
-#   Separamos las coordenadas en dos arrays, X e Y
+#  Separamos las coordenadas en dos arrays, X e Y
 X = valores[:, :-1]
 Y = valores[:, -1]
 
-#   Obtenemos las dimensiones de la matriz X (n x m)
+#  Obtenemos las dimensiones de la matriz X (n x m)
 filas = np.shape(X)[0]
 columnas = np.shape(X)[1]
 
-#   Funcion de normalizacion
-def normalize(X):
-    mu = []
-    des = []
-    x_norm = np.empty_like(X)
-    for i in range(columnas):
-        #   Calculamos la media y la desviacion
-        mu.append(np.mean(X[:,i]))
-        des.append(np.std(X[:,i]))
-        #   Calculamos el valor normalizado
-        for k in range(filas):
-            x_norm[k][i] = (X[k][i] - mu[i]) / des[i]
-    return x_norm, mu, des
 
-# Definicion de variables
+def normalize(X):
+    '''Normaliza los datos de la matriz X
+    restandoles su media y dividiendolos por la desviacion estandar'''
+    mu = np.mean(X, axis=0)
+    X_mean = X - mu
+    sigma = np.std(X, axis=0)
+    X_norm = X_mean / sigma
+    return X_norm, mu, sigma
+
+
+# Normalizamos los datos
 theta = [0] * (columnas + 1)
-alpha = 0.1
-X_norm, mu, sigma = normalize(X)
+
+valores_norm, mu, sigma = normalize(valores)
+X_norm = valores_norm[:, :-1]
+Y_norm = valores_norm[:, -1]
+
+
+# Añadimos una columna de 1s a X
 X_norm = np.hstack([np.ones([filas,1]),X_norm])
-X = np.hstack([np.ones([filas,1]),X])
 
 #   Funcion de coste
 def fun_coste(X,Y,theta):
@@ -51,24 +52,54 @@ def gradiente(X, Y, theta, alpha):
     for i in range(columnas):
         aux = ((np.dot(X, theta) - Y) * X[:, i])
         th[i] -= ((alpha/filas) * aux.sum())
-    return th
+    return th, fun_coste(X, Y, th)
 
-#θ= (XTX)−1XT~y   
+# Ecuacion normal
 def ecuacion_normal(X, Y):
-    
+    X= np.hstack([np.ones([filas,1]),X])
     th = np.linalg.inv(np.transpose(X).dot(X)).dot(np.transpose(X)).dot(Y)
     return th
 
-thg = gradiente(X_norm, Y, theta, alpha)
+# Pruebas con alpha
+for alpha in [0.3, 0.1, 0.03, 0.01]:
+    theta = np.zeros(columnas + 1)
+    costes = []
+    plt.figure(1)
+    plt.title(alpha)
+    plt.xlabel('Iteracion')
+    plt.ylabel('Coste')
+    
+    for i in range(3000):
+        Theta, cost = gradiente(X_norm, Y_norm, theta, alpha)
+        costes.append(cost)
+    
+    plt.plot(costes)
+    plt.show()
+    print('Coste final:', cost)
+    print('Theta final:', theta)
+
+# Comprobacion de las funciones
+# Calculamos las thetas
+thg = np.zeros(columnas + 1)
+alpha = 0.1
+
+for i in range(3000):
+    thg, cost = gradiente(X_norm, Y_norm, thg, alpha)
+
 the = ecuacion_normal(X, Y)
-metros = 1650
-habitaciones = 3
-metros_normalizados = (metros - mu[0]) / sigma[0]
-habitaciones_normalizadss = (habitaciones - mu[1]) / sigma[1]
-precio1 = thg[0] + thg[1]*metros_normalizados + thg[2]*habitaciones_normalizadss
-precio2 = the[0] + the[1]*metros + the[2]*habitaciones
-print(precio1, precio2)
 
+# Creamos datos de prueba y los normalizamos
+casa = np.array([1650,3]) 
+casa_norm = (casa - mu[:-1])
+casa_norm = casa_norm / sigma[:-1]
 
+# Prediccion con descenso del gradiente
+gradient_predict = np.matmul(np.append(np.array([1]),casa_norm), thg) * sigma[-1]
+gradient_predict = gradient_predict + mu[-1]
 
+# Prediccion con ecuación normal
+normal_predict = np.matmul(np.append(np.array([1]),casa), the)
 
+print('Precio con', casa[0], 'pies cuadrados y', casa[1], 'habitaciones:')
+print('Resultado con descenso del gradiente:', gradient_predict)
+print('Resultado con ecuación normal:', normal_predict)
